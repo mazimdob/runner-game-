b22097b90e587de11e817ca29cd74c485ec66a12
mazimdob <maxim.dobrov@icloud.com>
2026-05-09 13:50:15 +0100

    Add files via upload

--- runner.py
+++ runner.py
@@ -8,68 +8,96 @@ player.pos=50,HEIGHT//2
 scaryimages=["reaper.png","doll.png","ghost1.png","ghost2.png"]
 enemys=[]
 blade=[]
+speed=10
+highscore=0
 gamestate="start"
 score=0
 lives=3
 def create_enemy():
-    ship=Actor("ghostship.png")
-    ship.pos=WIDTH,random.randint(0,HEIGHT)
-    enemys.append(ship)
-    enemy=Actor(random.choice(scaryimages))
-    enemy.pos=WIDTH,ship.y
-    enemys.append(enemy)
+    if gamestate=="play":
+        ship=Actor("ghostship.png")
+        ship.pos=WIDTH,random.randint(0,HEIGHT)
+        enemys.append(ship)
+        enemy=Actor(random.choice(scaryimages))
+        enemy.pos=WIDTH,ship.y
+        enemys.append(enemy)
     
 def draw():
-    screen.blit("spookyocean.jpeg",(0,0))
-    player.draw()
+    
+    
     if gamestate=="start":
+        screen.fill("blue")
         screen.draw.text("press space to start \n press space to shoot the enemys \n shooting ship is 2 points \n shooting enemy is 1 point \n you have 3 lives \n every time enemy or ship hits player you lose 1 life  ",center=(WIDTH/2,HEIGHT/2),fontsize=30)
-    for enemy in enemys:
-        enemy.draw()
-    for b in blade:
-        b.draw()
-    screen.draw.text(f"score->{score}",(50,50),fontsize=30)
-    screen.draw.text(f"lives->{lives}",(WIDTH-300,50),fontsize=30)
+    
+    if gamestate=="play":
+        screen.blit("spookyocean.jpeg",(0,0))
+        player.draw()
+        for enemy in enemys:
+
+
+            enemy.draw()
+        for b in blade:
+            b.draw()
+        screen.draw.text(f"score->{score}",(50,50),fontsize=30)
+        screen.draw.text(f"lives->{lives}",(WIDTH-300,50),fontsize=30)
+        screen.draw.text(f"highscore->{highscore}",(WIDTH//2,50),fontsize=30)
+    if gamestate=="end":
+        screen.fill("red")
+        screen.draw.text("Game Over \n press space to restart",center=(WIDTH/2,HEIGHT/2),fontsize=50)
 
 def update():
-    global score,lives,enemys
-    if keyboard.up:
-        player.y-=10
-    if keyboard.down:                       
-        player.y+=10
-    if player.y<0:
-        player.y=HEIGHT
-    if player.y>HEIGHT:
-        player.y=0
-   
-    for b in blade:
-        b.x+=10
-        if b.x>WIDTH:
-            blade.remove(b)
-        for e in enemys:
-            if b.colliderect(e):
-                if e.image=="ghostship.png":
-                    print("ghost")
-                    score=score+2
+    global score,lives,enemys,gamestate,speed,highscore
+
+    if gamestate=="play":
+        speed+=0.005
+        if score>highscore:
+            highscore=score
+        if keyboard.up:
+            player.y-=speed
+        if keyboard.down:                       
+            player.y+=speed
+        if player.y<0:
+            player.y=HEIGHT
+        if player.y>HEIGHT:
+            player.y=0
+    
+        for b in blade:
+            b.x+=speed
+            if b.x>WIDTH:
+                blade.remove(b)
+            for e in enemys:
+                if b.colliderect(e):
+                    if e.image=="ghostship.png":
+                        print("ghost")
+                        score=score+2
+                        enemys=[]
+                    else:
+                        score=score+1
+                        blade.remove(b)
+                        enemys.remove(e)
+                    break
+        for enemy in enemys:
+            enemy.x-=speed
+            if enemy.x<-150:
+                enemys.remove(enemy)
+            if player.colliderect(enemy):
+            
+                lives-=1
+                if lives==0:
+                    gamestate="end"
+                if enemy.image=="ghostship.png":
                     enemys=[]
                 else:
-                    score=score+1
-                    blade.remove(b)
-                    enemys.remove(e)
-                break
-    for enemy in enemys:
-        enemy.x-=10
-        if enemy.x<-150:
-            enemys.remove(enemy)
-        if player.colliderect(enemy):
-            lives-=1
-            if enemy.image=="ghostship.png":
-                enemys=[]
-            else:
-                enemys.remove(enemy)
+                    enemys.remove(enemy)
 
 def on_key_down(key):
-     if key==keys.SPACE:
+     global gamestate,score,lives,speed
+     if key==keys.SPACE and gamestate!="play":
+        gamestate="play"
+        score=0
+        lives=3
+        speed=10
+     if key==keys.SPACE  and gamestate=="play":
         if(len(blade)<3):
             b=Actor("blade.png")
             b.pos=player.x+50,player.y
