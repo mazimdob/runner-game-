deb92d73dbbf6bca450e8f5404c4161be2deae1e
mazimdob <maxim.dobrov@icloud.com>
2026-05-10 09:59:10 +0100

    Add files via upload

--- runner.py
+++ runner.py
@@ -13,6 +13,8 @@ highscore=0
 gamestate="start"
 score=0
 lives=3
+music.play("backgroundsound")
+music.set_volume(0.05)
 def create_enemy():
     if gamestate=="play":
         ship=Actor("ghostship.png")
@@ -29,7 +31,7 @@ def draw():
         screen.fill("blue")
         screen.draw.text("press space to start \n press space to shoot the enemys \n shooting ship is 2 points \n shooting enemy is 1 point \n you have 3 lives \n every time enemy or ship hits player you lose 1 life  ",center=(WIDTH/2,HEIGHT/2),fontsize=30)
     
-    if gamestate=="play":
+    elif gamestate=="play":
         screen.blit("spookyocean.jpeg",(0,0))
         player.draw()
         for enemy in enemys:
@@ -41,7 +43,7 @@ def draw():
         screen.draw.text(f"score->{score}",(50,50),fontsize=30)
         screen.draw.text(f"lives->{lives}",(WIDTH-300,50),fontsize=30)
         screen.draw.text(f"highscore->{highscore}",(WIDTH//2,50),fontsize=30)
-    if gamestate=="end":
+    elif gamestate=="end":
         screen.fill("red")
         screen.draw.text("Game Over \n press space to restart",center=(WIDTH/2,HEIGHT/2),fontsize=50)
 
@@ -67,6 +69,7 @@ def update():
                 blade.remove(b)
             for e in enemys:
                 if b.colliderect(e):
+                    sounds.dyingsound.play()
                     if e.image=="ghostship.png":
                         print("ghost")
                         score=score+2
@@ -84,6 +87,7 @@ def update():
             
                 lives-=1
                 if lives==0:
+                    sounds.gameoversound.play()
                     gamestate="end"
                 if enemy.image=="ghostship.png":
                     enemys=[]
@@ -93,12 +97,14 @@ def update():
 def on_key_down(key):
      global gamestate,score,lives,speed
      if key==keys.SPACE and gamestate!="play":
+        sounds.startsound.play()
         gamestate="play"
         score=0
         lives=3
         speed=10
      if key==keys.SPACE  and gamestate=="play":
         if(len(blade)<3):
+            sounds.shotsound.play()
             b=Actor("blade.png")
             b.pos=player.x+50,player.y
             blade.append(b)
